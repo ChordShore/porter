28e8cda5bc1473ba3775d63ee992c43e0722b00d
ChordShore <contact@chordshore.com>
2025-08-12 14:55:38 -0700

    Added Image & Link Splitter functions

--- src/split_nodes_image.py
+++ src/split_nodes_image.py
@@ -1,17 +1,34 @@
 from obj_textnode import *
+from extract_markdown_images import *
 
 def split_nodes_image(old_nodes):
 	new_nodes = []
 	for node in old_nodes:
-		new_text_list = node.text.split(delimiter)
-		if len(new_text_list) % 2 == 0:
-			raise ValueError("Closing delimiter not found, invalid Markdown syntax!")
-		else:
-			for i in range(len(new_text_list)):
-				if new_text_list[i] == "":
-					continue
-				if i % 2 != 0:
-					new_nodes.append(TextNode(new_text_list[i], text_type))
-				else:
-					new_nodes.append(TextNode(new_text_list[i], TextType.TEXT))
+		found_images = extract_markdown_images(node.text)
+		if len(found_images) < 1:
+			new_nodes.append(node)
+			continue
+
+		new_text_list = []
+		temp_node_text = node.text
+		while len(temp_node_text) > 0:
+			for link in found_images:
+				if link[0] in temp_node_text:
+					split_text = temp_node_text.split(f"![{link[0]}]({link[1]})", 1)
+					new_text_list.append(split_text[0])
+					new_text_list.append(link)
+					if len(split_text) > 1:
+						temp_node_text = split_text[1]
+					else:
+						break
+
+		for i in range(len(new_text_list)):
+			if new_text_list[i] == "":
+				continue
+			#print("New Text List: ", new_text_list[i])
+			if new_text_list[i] in found_images:
+				new_nodes.append(TextNode(new_text_list[i][0], TextType.IMAGE, new_text_list[i][1]))
+			else:
+				new_nodes.append(TextNode(new_text_list[i], TextType.TEXT))
+				
 	return new_nodes
--- src/split_nodes_link.py
+++ src/split_nodes_link.py
@@ -6,26 +6,29 @@ def split_nodes_link(old_nodes):
 	for node in old_nodes:
 		found_links = extract_markdown_links(node.text)
 		if len(found_links) < 1:
-			return [node]
+			new_nodes.append(node)
+			continue
 
 		new_text_list = []
 		temp_node_text = node.text
 		while len(temp_node_text) > 0:
 			for link in found_links:
 				if link[0] in temp_node_text:
-					split_text = temp_node_text.split(f"![{link[0]}]({link[1]})", 1)
+					split_text = temp_node_text.split(f"[{link[0]}]({link[1]})", 1)
 					new_text_list.append(split_text[0])
 					new_text_list.append(link)
-			if len(split_text) > 1:
-				temp_node_text = split_text[1]
-			else:
-				break
+					if len(split_text) > 1:
+						temp_node_text = split_text[1]
+					else:
+						break
 
 		for i in range(len(new_text_list)):
 			if new_text_list[i] == "":
 				continue
-			if new_text_list[i] is str:
-				new_nodes.append(TextNode(new_text_list[i], TextType.TEXT))
-			else:
+			#print("New Text List: ", new_text_list[i])
+			if new_text_list[i] in found_links:
 				new_nodes.append(TextNode(new_text_list[i][0], TextType.LINK, new_text_list[i][1]))
+			else:
+				new_nodes.append(TextNode(new_text_list[i], TextType.TEXT))
+				
 	return new_nodes
