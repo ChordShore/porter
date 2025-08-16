fd12b263a4196029c68d179b579d469c98d5a482
ChordShore <contact@chordshore.com>
2025-08-15 18:45:26 -0700

    updating basepath, attempt 2

--- src/main.py
+++ src/main.py
@@ -3,20 +3,18 @@ import sys
 from deploy_static_to_public import *
 from generate_page_recursive import *
 
+dir_path_cont = "./content/"
+template_path = "./template.html"
+dir_path_dest = "./docs/"
+default_basepath = "/"
+
 def main():
 
-    print(sys.argv)
-    basepath = sys.argv[1]
-    print(basepath)
-    if basepath is None:
-        basepath = "/"
-
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+        
     deploy_static_to_public()
-    dir_path_cont = "../porter/content/"
-    template_path = "../porter/template.html"
-    dir_path_dest = "../porter/docs/"
     generate_page_recursive(dir_path_cont, template_path, dir_path_dest, basepath)
-
-    #print(basepath)
 
 main()
